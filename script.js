91d6cf5edd0a73bde73b338cd3a76e296b7b8865
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2022-12-27 11:32:42 -0500

    project 5 final commit

--- script.js
+++ script.js
@@ -39,9 +39,98 @@ async function getRandomUser(){
 }
 
 
+//using mapp function
+function doubleMoney(){
+    data = data.map(user => {
+        return {...user, money: user.money*2} 
+    });
+    updateDOM();
+}
+
+//Sort the users by the ammount
+function sortBy(){
+    data.sort((a,b)=>b.money-a.money);
+    updateDOM();
+}
+
+//filter the data to show some criteria
+function showFiltered(){
+    data = data.filter(user => user.money>1000000);
+    updateDOM();
+}
+
+//Calcular el valor total
+function calculateWealth(){
+    const wealth =data.reduce((acum, user)=>(acum += user.money),0);
+    const wealthEl =document.createElement('div');
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthEl);
+}
+
 function addData(obj){
     data.push(obj);
+
+    updateDOM();
+}
+
+//Función con parámetro opcional para actualizar el Div en el DOM
+function updateDOM(providedDAta=data){
+    //Clear the main div
+    main.innerHTML = '<h2><strong>Person</strong> Amount</h2>';
+
+    providedDAta.forEach(item=>{
+        const element = document.createElement('div');
+        element.classList.add('person');
+        element.innerHTML = `<strong>${item.name}</strong>     ${formatMoney(item.money)}`;
+        main.appendChild(element);
+    });
+
+}
+
+// Format number as money
+function formatMoney(number){
+    return '$ ' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 }
 
 
 
+
+//Event listener
+addUsertBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortBy);
+showMillionairesBtn.addEventListener('click', showFiltered);
+calculateWealthBtn.addEventListener('click', calculateWealth)
+
+/*
+Ejemplo de un map
+
+const arr1 =[1,2,3,4,5];
+const arr2 = arr1.map(item => item*2)
+Output:
+[2,4,6,8,10]
+
+*/
+
+
+/*
+Ejemplo de un filter
+
+cons arr = [20,23,25,30,21, 50,60];
+
+const under30 = arr.filter(funtion(item){
+    return item<30;
+})
+
+*/
+
+
+/*
+Ejemplo de uso de reduce
+
+const arr = [1,2,3,4,5];
+
+const total = arr.reduce((acum, num)=>(acum + num),0)
+Output : 15
+
+*/
